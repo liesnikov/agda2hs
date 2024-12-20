7815317843625434d5923fd503b0b1997c498a84
Bohdan <17168103+liesnikov@users.noreply.github.com>
2024-12-20 10:12:02 +0100

    more silly

--- nix/shell.nix
+++ nix/shell.nix
@@ -2,11 +2,15 @@
   pkgs ? import <nixpkgs> { },
   agda2hs-hs ? (import ./lib.nix { inherit pkgs; }).agda2hs-hs,
 }:
-agda2hs-hs.env.overrideAttrs (prev: {
-  nativeBuildInputs = prev.nativeBuildInputs ++ (with pkgs.haskellPackages; [
+let
+  hpkgs = pkgs.haskellPackages.extend (final: prev: {agda2hs = agda2hs-hs});
+in
+hpkgs.shellFor {
+  packages = p: [ p.agda2hs ];
+  nativeBuildInputs = with pkgs.haskellPackages; [
     cabal-install
     cabal2nix
     haskell-language-server
     pkgs.agda
-  ]);
-})
+  ];
+}
