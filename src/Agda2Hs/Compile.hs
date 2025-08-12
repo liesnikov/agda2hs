e2050dc2a997c90b0412f1ee6622ab85f760ceb2
liesnikov <17168103+liesnikov@users.noreply.github.com>
2025-08-12 12:37:47 +0200

    Add debug prints for current options and enable imports

--- src/Agda2Hs/Compile.hs
+++ src/Agda2Hs/Compile.hs
@@ -28,7 +28,7 @@ import Agda2Hs.Compile.Function ( compileFun, checkTransparentPragma, checkInlin
 import Agda2Hs.Compile.Name ( hsTopLevelModuleName )
 import Agda2Hs.Compile.Postulate ( compilePostulate )
 import Agda2Hs.Compile.Record ( compileRecord, checkUnboxPragma, checkTuplePragma )
-import Agda2Hs.Compile.RuntimeCheckUtils ( importDec )
+import Agda2Hs.Compile.RuntimeCheckUtils ( importDec, showOptions )
 import Agda2Hs.Compile.Types
 import Agda2Hs.Compile.Utils
 import Agda2Hs.Pragma
@@ -80,11 +80,10 @@ compile
   :: Options -> ModuleEnv -> IsMain -> Definition
   -> TCM (RtcDefs, CompileOutput)
 compile opts tlm _ def = do
-  --when rtc importDec
   withCurrentModule (qnameModule qname)
     $ runC tlm rtc (optRewrites opts)
     $ setCurrentRangeQ qname
-    $ compileAndTag <* postCompile
+    $ (when rtc (liftTCM importDec)) *> compileAndTag <* postCompile
   where
     qname = defName def
     rtc = optRtc opts
@@ -95,7 +94,9 @@ compile opts tlm _ def = do
     compileAndTag :: C RtcDefs
     compileAndTag = (tag <$>) <$> do
       p <- processPragma qname
-
+      pure $ setCommandLineOptions . stPersistentOptions . stPersistentState =<< getTC
+      currentOptions <- useTC stPragmaOptions
+      reportSDoc "agda2hs.compile.import" 25 $ "Current options are" <+> showOptions currentOptions
       reportSDoc "agda2hs.compile" 5  $ text "Compiling definition:" <+> prettyTCM qname
       reportSDoc "agda2hs.compile" 45 $ text "Pragma:" <+> text (show p)
       reportSDoc "agda2hs.compile" 45 $ text "Compiling definition:" <+> pretty (theDef def)
